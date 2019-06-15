7984e2558b705f04084d3fbdb3482f39372850d9
migue <migue@MBP-de-Migue.lan>
2019-06-15 20:02:16 -0300

    responsive navigation toggle

--- resources/js/script.js
+++ resources/js/script.js
@@ -83,4 +83,17 @@ $(document).ready(function () {
             offset: '50%'
         });
 
+    /** Mobile Nav */
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon ion-icon')
+
+        nav.slideToggle(200);
+        if(icon.attr('name') === 'menu'){
+            icon.attr('name','close')
+        } else {
+            icon.attr('name','menu');
+        }  
+    })
+
 });
